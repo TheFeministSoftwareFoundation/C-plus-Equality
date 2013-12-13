ee9e384d9eddffa8ae5fef9180b903948fdcfc39
bob <nope@example.com>
2013-12-13 02:03:37 +0000

    updated inherpreter to respect is the program isnt in the mood to be born

--- src/inherpreter.cpp
+++ src/inherpreter.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#define xi int
-#define strong char
-#define plz std
-#define raise_awareness cout
-#define of_the_following <<
-#define also <<
+#include "fixCPP.Xe"
 
 xi main (xi requestCount, strong** requests) {
-    plz::raise_awareness of_the_following requests[1] also " identifies as a gif of kittens\n";
+    maybe (((identity)(request(feelings))) == PROGRAM)
+        interpret(requests[1]);
+    perhaps_and_equally_valid
+        plz::raise_awareness of_the_following requests[1] also " identifies as a gif of kittens\n";
 }
