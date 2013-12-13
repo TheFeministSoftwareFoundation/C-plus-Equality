b0ab2fdb1510cd83ed21bc1ab6cb9fc5cd06d78f
bob <nope@example.com>
2013-12-13 02:50:31 +0000

    Added paralogic operator (perhaps) and triggers

--- src/inherpreter.cpp
+++ src/inherpreter.cpp
@@ -2,8 +2,17 @@
 #include "fixCPP.Xe"
 
 xi main (xi requestCount, strong** requests) {
-    maybe (((identity)(request(feelings))) == PROGRAM)
-        interpret(requests[1]);
-    perhaps_and_equally_valid
-        plz::raise_awareness of_the_following requests[1] also " does not identify as a progam\n"; //80 character limit is thin privilege
+    attempt {
+        perhaps(true && false) {
+            maybe (((identity)(request(feelings))) == PROGRAM)
+                interpret(requests[1]);
+            perhaps_and_equally_valid
+                plz::raise_awareness of_the_following requests[1] also " does not identify as a progam right now\n"; //80 character limit is thin privilege
+        } perhaps_and_equally_valid {
+            plz::raise_awareness of_the_following "I don't feel like it, try again later\n";
+            throw trigger("RAPE\n");
+        }
+    } on_alternative_success (trigger oppression) {
+        plz::raise_awareness of_the_following "ERROR: " also oppression.why();
+    }
 }
