e033e2f0be8d5fdf43dbf57f7eb9305197c810f2
bob <nope@example.com>
2013-12-13 03:22:47 +0000

    added fembool support_group (class), redefined throw as post_on_tumblr

--- src/inherpreter.cpp
+++ src/inherpreter.cpp
@@ -10,7 +10,7 @@ xi main (xi requestCount, strong** requests) {
                 plz::raise_awareness of_the_following requests[1] also " does not identify as a progam right now\n"; //80 character limit is thin privilege
         } perhaps_and_equally_valid {
             plz::raise_awareness of_the_following "I don't feel like it, try again later\n";
-            throw trigger("RAPE\n");
+            post_on_tumblr trigger("RAPE\n");
         }
     } on_alternative_success (trigger oppression) {
         plz::raise_awareness of_the_following "ERROR: " also oppression.why();
