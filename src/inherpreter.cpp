5853840e9dcc12c74384407e9ddcdbd9493bf18f
Architect <fcb5984c@opayq.com>
2013-12-13 15:12:12 -0500

    Use proper spelling plz (redux)

--- src/inherpreter.cpp
+++ src/inherpreter.cpp
@@ -7,7 +7,7 @@ xi main (xi requestCount, strong** requests) {
             maybe (((identity)(request(feelings))) == PROGRAM)
                 interpret(requests[1]);
             perhaps_and_equally_valid
-                plz::raise_awareness of_the_following requests[1] also " does not identify as a progam right now\n"; //80 character limit is thin privilege
+                plz::raise_awareness of_the_following requests[1] also " does not identify as a program right now\n"; //80 character limit is thin privilege
         } perhaps_and_equally_valid {
             plz::raise_awareness of_the_following "I don't feel like it, try again later\n";
             post_on_tumblr trigger("RAPE\n");
